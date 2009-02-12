729a8fe74893e16c70d3a9e1e17f661e77a4d3ed
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2009-02-12 00:57:10 +0000

    Added the UserService interface.

--- src/main/java/br/com/arsmachina/authentication/service/UserService.java
+++ src/main/java/br/com/arsmachina/authentication/service/UserService.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package br.com.arsmachina.authentication.controller.service;
+package br.com.arsmachina.authentication.service;
 
 import br.com.arsmachina.authentication.entity.User;
 
@@ -27,7 +27,7 @@ public interface UserService {
 	/**
 	 * Returns the user using the application in this thread.
 	 * 
-	 * @return
+	 * @return an {@link User} or null.
 	 */
 	User getUser();
 
